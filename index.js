ab1dfd8d571a15cd1bcacbc0c2bf043dcfe5dd7e
tscotce <tscotce@gmail.com>
2017-11-18 16:16:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 function scuberGreetingForFeet (distance) {
   if (distance <= 400){
     return 'This one is on me!';
-  }
   else if (distance > 2000){
     return 'I will gradly take your thirty bucks.';
   }
