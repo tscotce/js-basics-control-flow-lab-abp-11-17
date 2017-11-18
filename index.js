da8c7bdb50d56c20a471c183845744c889ca0b17
tscotce <tscotce@gmail.com>
2017-11-18 16:49:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -31,9 +31,11 @@ function switchOnCharmFromTip (tip) {
     case 'generous':
       return 'Thank you so much.'
       break;
-      case 'not as generous':
+    case 'not as generous':
       return 'Thank you.'
       break;
-    default: 'Bye.'
+    case 'thanks for everything'
+    return 'Bye.'
+    break;      
   }
 }
